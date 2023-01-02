236406f4b689599e67c1ca0dd8be90682428a7aa
kris <krishint29@gmail.com>
2023-01-03 02:10:23 +0530

    first commit

---
+++ apparel-ecom/src/main/java/com/example/apparelecom/ApparelEcomApplication.java
@@ -0,0 +1,13 @@
+package com.example.apparelecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApparelEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApparelEcomApplication.class, args);
+	}
+
+}
---
+++ apparel-ecom/src/test/java/com/example/apparelecom/ApparelEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apparelecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApparelEcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
